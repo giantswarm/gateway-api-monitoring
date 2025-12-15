327f8be25204a17c086cb1b972e6ef84861c2ede
Matías Charrière <matias@giantswarm.io>
2025-12-15 14:03:26 +0100

    Initialize repo (#1)
    * initial commit
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * setup vendir
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * Add sync script and run it
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * Add envoy-gateway dashboards
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * add hack/merger to deal with merges in Json files
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * Add patch for envoy-clusters dashboard
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * merge by name, id and refId
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * Patch envoy-clusters.json
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    ---------
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

---
+++ sync/sync.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) ; readonly dir
+cd "${dir}/.."
+
+# Build merger tool
+make -C hack/merger
+
+# Stage 1 sync
+set -x
+vendir sync
+{ set +x; } 2>/dev/null
+
+# Remove trailing whitespace end of lines (hack to fix vendir bug)
+find vendor/ -type f -exec sed -i 's/[[:space:]]*$//' {} \;
+
+# Patches
+./sync/patches/dashboards-envoy-gateway/patch.sh
+
