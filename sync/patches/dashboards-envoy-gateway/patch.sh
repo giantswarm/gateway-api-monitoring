29a2fe52f17c5dc6c24049040177a9a4083625d0
Matías Charrière <matias@giantswarm.io>
2025-12-16 10:16:42 +0100

    Patch Envoy Gateway dashboards (#5)
    * patch envoy-proxy-global dashboard
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * patch envoy-gateway-global
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    ---------
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

--- sync/patches/dashboards-envoy-gateway/patch.sh
+++ sync/patches/dashboards-envoy-gateway/patch.sh
@@ -12,12 +12,13 @@ cd "${repo_dir}"
 readonly script_dir_rel=".${script_dir#"${repo_dir}"}"
 dashboard_base="${repo_dir}/dashboards/envoy-gateway"
 
-patch_file="${script_dir}/envoy-clusters-patch.json"
 merger="${repo_dir}/hack/merger/merger"
 
 set -x
 
 # Merge patch file into dashboard
-"${merger}" "${dashboard_base}/envoy-clusters.json" "${patch_file}"
+"${merger}" "${dashboard_base}/envoy-clusters.json" "${script_dir}/envoy-clusters-patch.json"
+"${merger}" "${dashboard_base}/envoy-gateway-global.json" "${script_dir}/envoy-gateway-global-patch.json"
+"${merger}" "${dashboard_base}/envoy-proxy-global.json" "${script_dir}/envoy-proxy-global-patch.json"
 
 { set +x; } 2>/dev/null
